7aec90cd097a8d79e31b9964b28bd29a1ad68973
coolcool.rio75@gmail.com <AA9009pl>
2020-06-11 13:36:50 +0700

    redeploy model v2, make the field nullable

--- scanner/models.py
+++ scanner/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Candi(models.Model):
     candi_url = models.TextField(blank=True, null=True)
-    candi_img = models.ImageField(upload_to='images/', blank=True, null=True)
+    candi_img = models.ImageField(upload_to='images/')
